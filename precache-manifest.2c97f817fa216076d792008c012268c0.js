cbfe6c29b030b9fb811eae2d7797b632363f23d5
wannasakon <wannasakon.ph.61@ubu.ac.th>
2020-10-28 23:47:42 +0700

    Updates

---
+++ precache-manifest.2c97f817fa216076d792008c012268c0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9e77b829e916a38ab0e0dec2ea8b1a8",
+    "url": "/com-math/index.html"
+  },
+  {
+    "revision": "d1487cb234fc7dec5d7f",
+    "url": "/com-math/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "ebdc447190d6635df6d7",
+    "url": "/com-math/static/css/main.b4898ebb.chunk.css"
+  },
+  {
+    "revision": "d1487cb234fc7dec5d7f",
+    "url": "/com-math/static/js/2.ec39922d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/com-math/static/js/2.ec39922d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebdc447190d6635df6d7",
+    "url": "/com-math/static/js/main.d78b9f2d.chunk.js"
+  },
+  {
+    "revision": "6b089cf9632bbe656f94",
+    "url": "/com-math/static/js/runtime-main.71c63b4f.js"
+  },
+  {
+    "revision": "6d75a71cf45b2cbe8d534ba79bef02ec",
+    "url": "/com-math/static/media/blackboard.6d75a71c.jpg"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/com-math/static/media/logo.ee7cd8ed.svg"
+  }
+]);
